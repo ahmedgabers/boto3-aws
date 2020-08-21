2806e69a6d94299121bb55c8a66b99e5307c3015
Ahmed Gaber <51884558+x300n@users.noreply.github.com>
2020-08-21 21:34:57 +0200

    Update Translate.py

--- Translate.py
+++ Translate.py
@@ -1,16 +1,11 @@
 import boto3
 
-client = boto3.client('translate')
+def translate_text(**kwargs):
+    client = boto3.client('translate')
+    response = client.translate_text(**kwargs)
 
-def translate_text(text, source_language_code, target_language_code):
-    response = client.translate_text(
-        Text = text,
-        SourceLanguageCode = source_language_code,
-        TargetLanguageCode = target_language_code
-
-    )
     print(response['TranslatedText'])
 
 
 if __name__ == "__main__":
-    translate_text('Translate this to french', 'en', 'fr')
+    translate_text(Text='Translate this to french', SourceLanguageCode='en', TargetLanguageCode='fr')
